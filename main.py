eb21bdeb42416cfa3e6edd6fb35dbe48a118599d
Yaroslav Loshkarev <n128a7@mail.ru>
2023-01-18 18:23:13 +0300

    Program with decorator for func work time are ready *add generate_sequence_of_numbers function for create tuple of number

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def get_time_of_execute_function(func):
-    def wrapper():
+    def wrapper(list, *items):
         import timeit
-        function_code = "func()"
+        function_code = f'''{func(list, *items)}'''
         return timeit.timeit(stmt=function_code, number=10000)
     return wrapper
 
@@ -19,3 +19,10 @@ def add_items_to_list_var2(list, *items):
     return list
 
 
+def generate_sequence_of_numbers():
+    import random
+    return tuple(random.randint(1, 1000000) for _ in range(100000))
+
+
+res1_time_value = add_items_to_list_var1(list(generate_sequence_of_numbers()), generate_sequence_of_numbers())
+res2_time_value = add_items_to_list_var2(list(generate_sequence_of_numbers()), generate_sequence_of_numbers())
