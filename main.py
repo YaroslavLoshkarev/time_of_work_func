43c64626570626d0d727438ae799ff55d9171d12
Yaroslav Loshkarev <n128a7@mail.ru>
2023-01-18 17:55:34 +0300

    Starting to use the decorator

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def get_time_of_execute_function(func):
     return wrapper
 
 
+@get_time_of_execute_function
 def add_items_to_list_var1(list, *items):
     list.clear()
     for item in items:
         list.append(item)
     return list
 
-
+@get_time_of_execute_function
 def add_items_to_list_var2(list, *items):
     list = [x for x in items]
     return list
 
 
-end = 1
