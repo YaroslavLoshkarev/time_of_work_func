bc77ce1c5b869524eca339c6bcdf473dc3b1041a
Yaroslav Loshkarev <n128a7@mail.ru>
2023-01-18 17:22:37 +0300

    create new functions

---
+++ main.py
@@ -0,0 +1,18 @@
+def get_time_of_execute_function(func):
+    def wrapper():
+        import timeit
+        function_code = "func()"
+        return timeit.timeit(stmt=function_code, number=10000)
+    return wrapper
+
+
+def add_items_to_list_var1(list, *items):
+    list.clear()
+    for item in items:
+        list.append(item)
+    return list
+
+
+def add_items_to_list_var2(list, *items):
+    list = [x for x in items]
+    return list
